6a9f8db67aabedd796c8162cf6605dcecb497919
Ben Bernstein <benb116@gmail.com>
2014-12-26 17:27:24 -0500

    Quote and IP recording

--- System.sh
+++ System.sh
@@ -1,4 +1,4 @@
-echo `scutil --get ComputerName`;
+echo "Ben's MacBook Pro";
 
 hour=`date "+%l:%M %p" | cut -d ':' -f 1`
 if [[ $hour -gt 9 ]]; 
@@ -10,28 +10,36 @@ fi
 
 date "+%A, %B %d, %Y"
 
-curl http://checkip.dyndns.org | cut -d ':' -f 2 | cut -d '<' -f 1 | cut -d ' ' -f 2
+IP=`curl http://checkip.dyndns.org | cut -d ':' -f 2 | cut -d '<' -f 1 | cut -d ' ' -f 2`
+echo $IP
+echo $IP > ~/Dropbox/Save/IP.txt
 
 ipconfig getifaddr en0
 
 top -l 1 | awk '/CPU usage/ {print $1 " " $2" " $3" "$4" "$5" "$6" "$7" "$8}' ;
-export memTotal=`sysctl -n hw.memsize | awk '{print $0/1048576}'`;
-export memUsed=`(top -l 1 | awk '/PhysMem/' | awk '{print $2}';) | cut -d 'M' -f 1  | cut -d 'G' -f 1`;
-export memFree=`echo $memTotal - $memUsed | bc`;
-export memShortFree=`echo "scale=2;$memFree / 1024" | bc -l`;
-export memShortUsed=`echo "scale=2;$memUsed / 1024" | bc -l`;
 
-echo Active memory: $memShortFree GB of RAM" "- "$(( 100* $memFree / $memTotal ))"%;
+THEN=$(sysctl kern.boottime | awk '{print $5}' | sed "s/,//")
+NOW=$(date +%s)
+DIFF=$(($NOW-$THEN))
 
-uptime | awk '{sub(/[0-9]|user,|users,|load/, "", $6);
-sub(/mins,|min,/, "min", $6);
-sub(/user,|users,/, "", $5); sub(",", "min", $5);
-sub(":", "h ", $5); sub(/[0-9]/, "", $4);
-sub(/day,/, " day ", $4); sub(/days,/, " days ", $4);
-sub(/mins,|min,/, "min", $4); sub("hrs,", "h", $4);
-sub(":", "h ", $3);
-sub(",", "min", $3);
-print "System Uptime: "$3$4$5$6}'
+DAYS=$(($DIFF/86400));
+DIFF=$(($DIFF-($DAYS*86400)))
+HOURS=$(($DIFF/3600))
+DIFF=$(($DIFF-($HOURS*3600)))
+MINUTES=$(($DIFF/60))
+
+function format {
+if [ $1 == 1 ]; then
+	echo $1 ' ' $2
+elif [ $1 == 0 ]; then
+	echo ''
+else
+	echo $1 ' ' $2's'
+fi
+}
+
+UPTIME=$(echo `format $DAYS "day"` `format $HOURS "hour"` `format $MINUTES "minute"` | sed 's/^ *//')
+echo "System uptime: $UPTIME"
 
 my_ac_adapt=`ioreg -w0 -l | grep ExternalConnected | awk '{print $5}'`
 if [ "$my_ac_adapt" == "Yes" ]
@@ -46,3 +54,9 @@ else
 fi
 
 osascript /Users/Ben/Dropbox/Developer/GeekTool/Position.scpt
+
+raw=`curl http://www.brainyquote.com/link/quotebr.js`
+quote=`echo $raw | cut -d '"' -f 4 | cut -d '<' -f 1`
+author=`echo $raw | cut -d '>' -f 6 | cut -d '<' -f 1`
+echo $quote
+echo '												 - '$author
